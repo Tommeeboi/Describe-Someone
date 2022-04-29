f81d30357b47df562c329b0f0b542c9f2261309b
Tommeeboi <97439104+Tommeeboi@users.noreply.github.com>
2022-04-29 08:15:32 +0100

    code is more neat

--- desc.js
+++ desc.js
@@ -1,4 +1,4 @@
-// why do this look so satisfying
+// why does this look so satisfying
 const name = document.getElementById("name");
 const looks = document.getElementById("looks");
 const smells = document.getElementById("smells");
@@ -11,14 +11,14 @@ const f = document.getElementById("f");
 const w = document.getElementById("w");
 
 // Submit button functionality
-
+// Version 2: The code is now more efficient and neat now
 sub.onclick = function() {
     if (name.value === '' || looks.value === '' || smells.value === '' || feels.value === '' || want.value === '') {
         alert("Make sure everything has a value!")
     } else {
-    l.innerHTML = name.value + " looks " + looks.value;
-    s.innerHTML = name.value + " smells " + smells.value;
-    f.innerHTML = name.value + " feels " + feels.value;
-    w.innerHTML = "I want to " + want.value;
+    l.innerHTML = `${name.value} looks ${looks.value}`;
+    s.innerHTML = `${name.value} smells ${smells.value}`;
+    f.innerHTML = `${name.value} feels ${feels.value}`;
+    w.innerHTML = `I want to ${want.value}`;
   }
 }
